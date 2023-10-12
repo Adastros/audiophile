3a64f417f43824cfe0d2f30148d58785181a9cb4
Danny <dannydo286@gmail.com>
2023-10-11 23:03:37 -0700

    Add feature to clear cart when home button clicked
    Closes #57

--- frontend/src/components/orderConfirmationModal/BackToHomeButton.js
+++ frontend/src/components/orderConfirmationModal/BackToHomeButton.js
@@ -1,9 +1,17 @@
 import { LinkOverlay, LinkBox, Button } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { clearCart } from '../../reducers/cartReducer';
 
 const BackToHomeButton = ({ buttonVariant, buttonSize, route }) => {
+  const dispatch = useDispatch();
+
+  const handleButtonClick = () => {
+    dispatch(clearCart());
+  };
+
   return (
-    <LinkBox w="100%">
+    <LinkBox w="100%" onClick={handleButtonClick}>
       <LinkOverlay as={ReactRouterLink} to={route}>
         <Button variant={buttonVariant} size={buttonSize}>
           BACK TO HOME
