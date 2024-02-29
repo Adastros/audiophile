166913739e0893c21b55f5e9421494801ca3a270
Danny <dannydo286@gmail.com>
2024-02-28 23:38:18 -0800

    Fix hamburger menu not closing when viewport >992px

--- frontend/src/components/header/Navigation.js
+++ frontend/src/components/header/Navigation.js
@@ -3,9 +3,22 @@ import MenuIconButton from './MenuIconButton';
 import NavLinks from '../NavLinks';
 
 const Navigation = ({ headerData, menuModal }) => {
-  const [isLargerThan992] = useMediaQuery('(width >= 992px)');
+  const [isLargerThan992px] = useMediaQuery('(width >= 992px)');
   const flexDir = { base: 'column', md: 'row' };
   const gaps = { base: '1rem', md: '2.125rem' };
+
+  const determineNavigation = () => {
+    if (isLargerThan992px) {
+      // Close menu when changing navigation elements
+      if (menuModal.isMenuModalOpen) {
+        menuModal.onMenuModalClose();
+      }
+
+      return <NavLinks flexDir={flexDir} gaps={gaps} />;
+    } else {
+      return <MenuIconButton headerData={headerData} menuModal={menuModal} />;
+    }
+  };
 
   return (
     <Box
@@ -13,11 +26,7 @@ const Navigation = ({ headerData, menuModal }) => {
       top={{ base: '0', lg: 'calc(50% - 12.5px)' }}
       left={{ base: '0', lg: 'calc(50% - 216px)' }}
     >
-      {isLargerThan992 ? (
-        <NavLinks flexDir={flexDir} gaps={gaps} />
-      ) : (
-        <MenuIconButton headerData={headerData} menuModal={menuModal} />
-      )}
+      {determineNavigation()}
     </Box>
   );
 };
